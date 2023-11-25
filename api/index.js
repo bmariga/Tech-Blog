9edeae98f3cd6df7d6b7765fa391a42be04cab56
mariga bruce <mariga.bn@gmail.com>
2023-11-25 18:36:57 +0300

    backend

--- api/index.js
+++ api/index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require("./models/User");
+const Post = require("./models/Post");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const app = express();
 const cookieParser = require("cookie-parser");
+const multer = require("multer");
+const uploadMiddleware = multer({ dest: "uploads/" });
+const fs = require("fs");
 
 const salt = bcrypt.genSaltSync(10);
 const secret = "8jkf9sD&23l1@%sG9zH0Yp$w7cXqBhU87";
@@ -70,14 +74,52 @@ app.post("/login", async (req, res) => {
 
 app.get("/profile", (req, res) => {
   const { token } = req.cookies;
+
+  if (!token) {
+    return res.status(401).json({ error: "Token not provided" });
+  }
+
   jwt.verify(token, secret, {}, (err, info) => {
-    if (err) throw err;
+    if (err) {
+      if (err.name === "TokenExpiredError") {
+        return res.status(401).json({ error: "Token has expired" });
+      }
+      return res.status(401).json({ error: "Invalid token" });
+    }
+
     res.json(info);
   });
 });
 
 app.post("/logout", (req, res) => {
-  res.cookie("token, ").json("ok");
+  res.cookie("token", "").json("ok");
+});
+
+app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
+  const { originalname, path } = req.file;
+  const parts = originalname.split(".");
+  const ext = parts[parts.length - 1];
+  const newPath = path + "." + ext;
+  fs.renameSync(path, newPath);
+
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { title, summary, content } = req.body;
+    const postDoc = await Post.create({
+      title,
+      summary,
+      content,
+      cover: newPath,
+      author: info.id,
+    });
+
+    res.json({ postDoc });
+  });
+});
+
+app.get("/post", async (req, res) => {
+  res.json(await Post.find());
 });
 
 app.listen(4000);
